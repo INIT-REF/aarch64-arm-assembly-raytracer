f5c7cc46e46fcd215bf59e92310d0eb93553c327
INIT-REF <rufmichan@freenet.de>
2025-12-13 19:11:43 +0100

    Update

--- src/main.s
+++ src/main.s
@@ -10,23 +10,22 @@
     depth = 50
 
     // camera setup
-    cam_center:      .float 0.0, 0.0, 0.0, 0.0
-    focal_length:    .float 1.0
-    viewport_height: .float 2.0
-
+    fov = 20 
+    look_from:  .float -2.0, 2.0, 1.0, 0.0
+    look_at:    .float 0.0, 0.0, -1.0, 0.0
+    v_up:       .float 0.0, 1.0, 0.0, 0.0
+    
     // include the scene
     .include "./src/world.inc"
 
     // some utility vectors
-    sky:    .float  0.5,  0.7,  1.0, 0.0
-    z_1:    .float  0.0,  0.0,  1.0, 0.0
-    
+    sky:    .float 0.5, 0.7, 1.0, 0.0
     not_1:  .float 0.999, 0.999, 0.999, 0.0
     _256:   .float 256.0, 256.0, 256.0, 0.0
 
     // t_min and t_max
     t_min: .float 0.001
-    t_max: .dword 0x7f800000
+    t_max: .word 0x7f800000
 
     // for near zero
     tiny: .float 0.0000003
@@ -36,21 +35,10 @@
     P3:   .ascii "P3\n"
     _255: .ascii "255\n"
 
-    // LUT for unsigned char to string
-    lut: .ascii "  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19 "
-         .ascii " 20  21  22  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39 "
-         .ascii " 40  41  42  43  44  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59 "
-         .ascii " 60  61  62  63  64  65  66  67  68  69  70  71  72  73  74  75  76  77  78  79 "
-         .ascii " 80  81  82  83  84  85  86  87  88  89  90  91  92  93  94  95  96  97  98  99 "
-         .ascii "100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 "
-         .ascii "120 121 122 123 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 139 "
-         .ascii "140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159 "
-         .ascii "160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 "
-         .ascii "180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 "
-         .ascii "200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 "
-         .ascii "220 221 222 223 224 225 226 227 228 229 230 231 232 233 234 235 236 237 238 239 "
-         .ascii "240 241 242 243 244 245 246 247 248 249 250 251 252 253 254 255 "
+    // look up tables
+    .include "./src/luts.inc"
 
+    // rendering done message
     done: .ascii "Rendering done, result in image.ppm\n"
 
 
@@ -181,7 +169,7 @@ multisample:
     // set ray.origin = camera_center
     ldr     q0, [x21, #8]
     str     q0, [x22]
-    
+ 
     // get random offset vector
     bl      random_offset
 
